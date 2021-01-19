d112fd5d9f60c87616dc114f0427c6b05e32c4cb
Tenzin Lekphell <tenzin.lekphell@ryerson.ca>
2021-01-18 19:47:06 -0500

    revised code

--- nintendo.js
+++ nintendo.js
@@ -5,22 +5,22 @@ const consoleName = document.querySelector('.console_name');
 const consoleType = document.querySelector('.console_type');
 const consoleDate = document.querySelector('.console_date');
 
-//Fetch bakery data using the async and await method
-//fetchBakeries function accepts a parameter
-const fetchBakeries = async(game) => {
+//Fetch Nintendo data using the async and await method
+//fetchNintendoData function accepts a parameter
+const fetchNintendoData = async(game) => {
   const api_call = await fetch(`https://nintendo-api-project.herokuapp.com/nintendo/${game}`);
   const data = await api_call.json();
   return { data }
 };
 
-//showData function uses the fetchBakeries function to fetch data
-//the argument used in fetchBakeries is the inputValue variable
+//showData function uses the fetchNintendoData function to fetch data
+//the argument used in fetchNintendoData is the inputValue variable
 //inputValue variable is the input field element retrieved by querySelector
 //inputValue.value is the current value in the input field
 //use a promise .then method to get a response back
 //write html inside the containers by using the ES6 string literal method
 const showData = () => {
-  fetchBakeries(inputValue.value).then((res) => {
+  fetchNintendoData(inputValue.value).then((res) => {
     console.log(res);
     consoleName.innerHTML = `Console Name: <span class="console_name">${res.data.id}</span>`;
     consoleType.innerHTML = `Console Type: <span class="console_type">${res.data.type}</span>`;
@@ -28,9 +28,9 @@ const showData = () => {
   })
 }
 
-//Select search bakeries button
+//Select search Nintendo button
 //Add a click event to the button and use the showData function 
-const searchBakeriesBtn = document.getElementById("getBakeries")
-searchBakeriesBtn.addEventListener("click",  () => {
+const searchNintendoBtn = document.getElementById("nintendo-data-btn")
+searchNintendoBtn.addEventListener("click",  () => {
   showData();
 });
